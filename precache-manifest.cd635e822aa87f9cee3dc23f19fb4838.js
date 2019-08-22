8f0f446e4909b3d26711ba5f6a7642168bd8eadf
abc <abc@example.com>
2019-08-22 13:11:56 +0300

    Updates

---
+++ precache-manifest.cd635e822aa87f9cee3dc23f19fb4838.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e478c52079050e009105c6fccab43ce2",
+    "url": "/MenuForRestaurant/index.html"
+  },
+  {
+    "revision": "505cfc64d33d16b54e6e",
+    "url": "/MenuForRestaurant/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e85af846b0cf061dbe5d",
+    "url": "/MenuForRestaurant/static/css/main.d768aff7.chunk.css"
+  },
+  {
+    "revision": "505cfc64d33d16b54e6e",
+    "url": "/MenuForRestaurant/static/js/2.f12894b2.chunk.js"
+  },
+  {
+    "revision": "e85af846b0cf061dbe5d",
+    "url": "/MenuForRestaurant/static/js/main.67e3106f.chunk.js"
+  },
+  {
+    "revision": "affa534b2283d648533d",
+    "url": "/MenuForRestaurant/static/js/runtime~main.aad87eb3.js"
+  },
+  {
+    "revision": "324b1341be0f53d9baba169399ea528e",
+    "url": "/MenuForRestaurant/static/media/burger.324b1341.png"
+  },
+  {
+    "revision": "de4bad759707a5a514fddfbd7338c6b9",
+    "url": "/MenuForRestaurant/static/media/coffee.de4bad75.png"
+  },
+  {
+    "revision": "324f1f481d029186678922133a74f8e0",
+    "url": "/MenuForRestaurant/static/media/dessert.324f1f48.png"
+  },
+  {
+    "revision": "fa1f933db054fada6c52008f6850b09f",
+    "url": "/MenuForRestaurant/static/media/french_fries.fa1f933d.png"
+  },
+  {
+    "revision": "3b4aa91816fbb0549671121fb0a488f3",
+    "url": "/MenuForRestaurant/static/media/nuggets.3b4aa918.png"
+  },
+  {
+    "revision": "cd39f4312e776ec9b75d4aa18b4b469d",
+    "url": "/MenuForRestaurant/static/media/roll.cd39f431.png"
+  }
+]);
